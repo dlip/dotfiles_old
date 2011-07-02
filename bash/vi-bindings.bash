97cb10c13e0d79cb92a9b68aa2b3f8c1878a7782
Dane Lipscombe <dane@lipscobme.com.au>
2011-07-03 00:01:20 +1000

    adding some vim bindings

---
+++ bash/vi-bindings.bash
@@ -0,0 +1,8 @@
+# ^l clear screen
+bind -m vi-insert "\C-l":clear-screen
+
+# ^p check for partial match in history
+bind -m vi-insert "\C-p":dynamic-complete-history
+
+# ^n cycle through the list of partial matches
+bind -m vi-insert "\C-n":menu-complete
